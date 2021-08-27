98c53478e3d00cf42d353ce7717c1c579799bdb2
Iyamperumal Rathinam <iyamperumal.r@gmail.com>
2021-08-27 20:37:49 +0000

    test fix

--- test/test-server.js
+++ test/test-server.js
@@ -27,7 +27,7 @@ describe('GET /', function () {
         if (err) {
           return done(err);
         }
-        chai.assert.isTrue(res.text.includes("<h1>Welcome to Peru's application</h1>"));
+        chai.assert.isTrue(res.text.includes("<h1>Welcome to Event Managers application</h1>"));
         return done();
       });
 
